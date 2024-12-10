4198d6bd29756456c9d40370a94e64e8a749eaf0
drodriguezventosa <work.dani.rodriguez@gmail.com>
2024-12-10 12:50:17 +0100

    Removed apply methods and refactor code

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class YellowTaxiData:
         self.jfk_df = pd.DataFrame()
         self.regular_df = pd.DataFrame()
         self.other_df = pd.DataFrame()
+        self.csv_df = pd.DataFrame()
 
 
     def import_data(self):
@@ -62,98 +63,34 @@ class YellowTaxiData:
         self.data = self.data[self.data['passenger_count'] > 0]
 
 
-    def generate_weeks(self):
-        df = pd.DataFrame(self.end_date_weeks, columns=['end_week'])
-        df['start_week'] = df['end_week'] - pd.to_timedelta(df['end_week'].dt.weekday, unit='D')
-
-        if df.loc[0]['start_week'] != self.start_date: # Remove week if start_date is the first day of week
-            df.drop(index=0, inplace=True)
-            df.reset_index(drop=True, inplace=True)
-
-        """if df['end_week'].iloc[-1] < pd.to_datetime(self.end_date):
-            df = pd.concat([
-                df,
-                pd.DataFrame({
-                    'start_week': [
-                        pd.to_datetime(self.end_date) - pd.to_timedelta(pd.to_datetime(self.end_date).weekday(),
-                                                                            unit='D')],
-                    'end_week': [pd.to_datetime(self.end_date)]
-                })
-            ], ignore_index=True)"""
-
-        self.weeks_ranges = df.copy()
-        self.weeks_ranges = self.weeks_ranges[['start_week', 'end_week']]
-
-        self.weeks_ranges['num_week'] = self.weeks_ranges['end_week'].dt.isocalendar().week.astype(str).str.zfill(3)
-
-
-    def generate_months(self):
-        date_range = pd.date_range(self.start_date, self.end_date, freq='MS')
-        dates_df = pd.DataFrame(date_range, columns=['date_dt'])
-
-        dates_df['start_day'] = dates_df['date_dt']
-        dates_df['end_day'] = dates_df['date_dt'] + pd.offsets.MonthEnd(0)
-        dates_df['date_dt'] = dates_df['date_dt'].dt.strftime('%Y-%m')
-
-        self.months_ranges = dates_df
+    def add_more_columns(self):
+        self.data['year_month'] = self.data['tpep_dropoff_datetime'].dt.strftime('%Y-%m')
+        self.data['year_dt'] = self.data['tpep_dropoff_datetime'].dt.year.astype(str)
+        self.data['week_dt'] = self.data['tpep_dropoff_datetime'].dt.isocalendar().week.astype(str).str.zfill(3)
+        self.data['year_week'] = self.data['year_dt'].str.cat(self.data['week_dt'], sep='-')
+        self.data['year_month_day'] = self.data['tpep_dropoff_datetime'].dt.strftime('%Y-%m-%d')
 
 
     def generate_week_metrics(self):
-        def get_total_services(row):
-            return self.data[(self.data['tpep_dropoff_datetime'] >= row['start_week']) & (
-                        self.data['tpep_dropoff_datetime'] <= row['end_week'])].shape[0]
-
-        def get_min_for_field(row, field):
-            return self.data[(self.data['tpep_dropoff_datetime'] >= row['start_week']) & (
-                    self.data['tpep_dropoff_datetime'] <= row['end_week'])][field].min()
-
-        def get_max_for_field(row, field):
-            return self.data[(self.data['tpep_dropoff_datetime'] >= row['start_week']) & (
-                    self.data['tpep_dropoff_datetime'] <= row['end_week'])][field].max()
-
-        def get_mean_for_field(row, field):
-            return self.data[(self.data['tpep_dropoff_datetime'] >= row['start_week']) & (
-                    self.data['tpep_dropoff_datetime'] <= row['end_week'])][field].mean()
-
         self.data['trip_time'] = self.data['tpep_dropoff_datetime'] - self.data['tpep_pickup_datetime']
         self.data['trip_time_in_seconds'] = self.data['trip_time'].dt.total_seconds()
 
-        # TODO: Change apply methods
-        self.weeks_ranges['min_trip_time'] = self.weeks_ranges.apply(
-            lambda x: get_min_for_field(x, 'trip_time_in_seconds'), axis=1
-        )
-        self.weeks_ranges['max_trip_time'] = self.weeks_ranges.apply(
-            lambda x: get_max_for_field(x, 'trip_time_in_seconds'), axis=1
-        )
-        self.weeks_ranges['mean_trip_time'] = self.weeks_ranges.apply(
-            lambda x: get_mean_for_field(x, 'trip_time_in_seconds'), axis=1
-        )
+        self.csv_df = self.data.groupby('year_week').agg(
+            min_trip_time=('trip_time_in_seconds', 'min'),
+            max_trip_time=('trip_time_in_seconds', 'max'),
+            mean_trip_time=('trip_time_in_seconds', 'mean'),
+            min_trip_distance=('trip_distance', 'min'),
+            max_trip_distance=('trip_distance', 'max'),
+            mean_trip_distance=('trip_distance', 'mean'),
+            min_trip_amount=('total_amount', 'min'),
+            max_trip_amount=('total_amount', 'max'),
+            mean_trip_amount=('total_amount', 'mean'),
+            total_services=('total_amount', 'count')
+        ).reset_index()
 
-        self.weeks_ranges['min_trip_distance'] = self.weeks_ranges.apply(
-            lambda x: get_min_for_field(x, 'trip_distance'), axis=1
-        )
-        self.weeks_ranges['max_trip_distance'] = self.weeks_ranges.apply(
-            lambda x: get_max_for_field(x, 'trip_distance'), axis=1
-        )
-        self.weeks_ranges['mean_trip_distance'] = self.weeks_ranges.apply(
-            lambda x: get_mean_for_field(x, 'trip_distance'), axis=1
-        )
-
-        self.weeks_ranges['min_trip_amount'] = self.weeks_ranges.apply(
-            lambda x: get_min_for_field(x, 'total_amount'), axis=1
-        )
-        self.weeks_ranges['max_trip_amount'] = self.weeks_ranges.apply(
-            lambda x: get_max_for_field(x, 'total_amount'), axis=1
-        )
-        self.weeks_ranges['mean_trip_amount'] = self.weeks_ranges.apply(
-            lambda x: get_mean_for_field(x, 'total_amount'), axis=1
-        )
-
-        self.weeks_ranges['total_services'] = self.weeks_ranges.apply(lambda x: get_total_services(x), axis=1)
-
-        self.weeks_ranges['percentage_variation'] = (
-            self.weeks_ranges['total_services'] - self.weeks_ranges['total_services'].shift(1)
-            ) / self.weeks_ranges['total_services'].shift(1) * 100
+        self.csv_df['percentage_variation'] = (
+            self.csv_df['total_services'] - self.csv_df['total_services'].shift(1)
+            ) / self.csv_df['total_services'].shift(1) * 100
 
 
     def generate_month_metrics(self):
@@ -162,39 +99,32 @@ class YellowTaxiData:
             'jfk_df': 2,
             'other_df': -1
         }
+
+        self.data['day_type'] = np.where(self.data['tpep_dropoff_datetime'].dt.dayofweek >= 5, 2, 1)
+        self.data['RatecodeID'] = self.data['RatecodeID'].astype(int)
+
         for rc_id in rate_code_id_dict.keys():
             attr = getattr(self, rc_id)
-            for row in self.months_ranges.itertuples(index=False):
-                month_df = self.data[(self.data['tpep_pickup_datetime'] >= row.start_day) &
-                                     (self.data['tpep_dropoff_datetime'] <= row.end_day)].copy()
 
-                month_df['day_type'] = np.where(month_df['tpep_dropoff_datetime'].dt.dayofweek >= 5, 2, 1)
-                month_df['RatecodeID'] = month_df['RatecodeID'].astype(int)
+            if rate_code_id_dict[rc_id] in [1, 2]:
+                df = self.data[self.data['RatecodeID'] == rate_code_id_dict[rc_id]]
+            else:
+                df = self.data[(self.data['RatecodeID'] != 1) & (self.data['RatecodeID'] != 2)]
 
-                if rate_code_id_dict[rc_id] in [1, 2]:
-                    df = month_df[month_df['RatecodeID'] == rate_code_id_dict[rc_id]]
-                else:
-                    df = month_df[(month_df['RatecodeID'] != 1) & (month_df['RatecodeID'] != 2)]
+            df = df[['year_month', 'day_type', 'trip_distance', 'passenger_count']]
 
-                df = df[['day_type', 'trip_distance', 'passenger_count']]
-                df['service'] = 1
+            df = df.groupby(['year_month', 'day_type']).agg(
+                services=('trip_distance', 'count'),
+                distances=('trip_distance', 'sum'),
+                passengers=('passenger_count', 'sum')
+            ).reset_index()
 
-                df = df.groupby('day_type').agg(
-                    services=('service', 'count'),
-                    distances=('trip_distance', 'sum'),
-                    passengers=('passenger_count', 'sum')
-                )
-
-                df['month'] = row.date_dt
-
-                attr = pd.concat([attr, df])
-                setattr(self, rc_id, attr)
+            attr = pd.concat([attr, df])
+            setattr(self, rc_id, attr)
 
 
     def format_data(self):
-        self.weeks_ranges = self.weeks_ranges.round(2)
-        self.weeks_ranges['year_week'] = self.weeks_ranges['end_week'].dt.isocalendar().year.astype(str) + '-' + \
-                                           self.weeks_ranges['num_week']
+        self.csv_df = self.csv_df.round(2)
 
         self.jfk_df = self.jfk_df.reset_index()
         self.regular_df = self.regular_df.reset_index()
@@ -202,16 +132,11 @@ class YellowTaxiData:
 
 
     def export_csv_data(self):
-        ordered_columns = ['year_week', 'min_trip_time', 'max_trip_time','mean_trip_time',
-            'min_trip_distance', 'max_trip_distance','mean_trip_distance', 'min_trip_amount', 'max_trip_amount',
-            'mean_trip_amount', 'total_services', 'percentage_variation'
-       ]
-        csv_weeks_ranges = self.weeks_ranges[ordered_columns]
-        csv_weeks_ranges.to_csv('processed_data.csv', sep='|', index=False)
+        self.csv_df.to_csv('processed_data.csv', sep='|', index=False)
 
 
     def export_excel_data(self):
-        common_columns = ['month', 'day_type', 'services', 'distances', 'passengers']
+        common_columns = ['year_month', 'day_type', 'services', 'distances', 'passengers']
         with pd.ExcelWriter("processed_data.xlsx", engine="openpyxl") as writer:
             self.jfk_df[common_columns].to_excel(writer, sheet_name="JFK", index=False)
             self.regular_df[common_columns].to_excel(writer, sheet_name="Regular", index=False)
@@ -224,36 +149,49 @@ class YellowTaxiData:
 
 
 if __name__ == '__main__':
-    start_time = time.perf_counter()
+    global_start_time = time.perf_counter()
 
+    print('Init objects ...')
+    start_time = time.perf_counter()
     yellow_taxi_data = YellowTaxiData(start_date='2022-01-01', end_date='2022-03-31')
+    print("*** {t} seconds ***".format(t=time.perf_counter() - start_time))
+
     print('Importing data ...')
+    start_time = time.perf_counter()
     yellow_taxi_data.import_data()
+    print("*** {t} seconds ***".format(t=time.perf_counter() - start_time))
 
     print('Cleaning data ...')
+    start_time = time.perf_counter()
     yellow_taxi_data.clean_data()
+    print("*** {t} seconds ***".format(t=time.perf_counter() - start_time))
 
-    print('Generating months ...')
-    yellow_taxi_data.generate_months()
-
-    print('Generating weeks ...')
-    yellow_taxi_data.generate_weeks()
+    print('Adding more columns ...')
+    start_time = time.perf_counter()
+    yellow_taxi_data.add_more_columns()
+    print("*** {t} seconds ***".format(t=time.perf_counter() - start_time))
 
     print('Generating week metrics ...')
+    start_time = time.perf_counter()
     yellow_taxi_data.generate_week_metrics()
+    print("*** {t} seconds ***".format(t=time.perf_counter() - start_time))
 
     print('Generating month metrics ...')
+    start_time = time.perf_counter()
     yellow_taxi_data.generate_month_metrics()
+    print("*** {t} seconds ***".format(t=time.perf_counter() - start_time))
 
     print('Formatting results ...')
+    start_time = time.perf_counter()
     yellow_taxi_data.format_data()
+    print("*** {t} seconds ***".format(t=time.perf_counter() - start_time))
 
     print('Exporting results ...')
+    start_time = time.perf_counter()
     yellow_taxi_data.export_data()
+    print("*** {t} seconds ***".format(t=time.perf_counter() - start_time))
 
-    end_time = time.perf_counter()
-
-    print("Execution time: {t} seconds".format(t=end_time - start_time))
+    print("Execution time: {t} seconds".format(t=time.perf_counter() - global_start_time))
 
 
 
